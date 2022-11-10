55f702e1119928f3ea51ce52eedd30bb9a8fceab
owentashlin <owentashlin@gmail.com>
2022-11-10 11:16:40 -0800

    updated routes to be behind oauth login

--- routes/projects.js
+++ routes/projects.js
@@ -1,17 +1,17 @@
 // routes/projects.js
 
-var express = require("express");
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
 
-var passport = require("passport");
-
+var passport = require("passport")
+const isLoggedIn = require("../config/auth")
 const projectsCtrl = require('../controllers/projects')
 
-router.get('/index', projectsCtrl.index)
-router.get('/projects/add', projectsCtrl.addProject)
-router.get('/projects/library', projectsCtrl.findAll)
-router.get('/projects/:id', projectsCtrl.showDetail)
-router.get('/projects/:id/update', projectsCtrl.showUpdate)
+router.get('/index', isLoggedIn, projectsCtrl.index)
+router.get('/projects/add', isLoggedIn, projectsCtrl.addProject)
+router.get('/projects/library', isLoggedIn, projectsCtrl.findAll)
+router.get('/projects/:id', isLoggedIn, projectsCtrl.showDetail)
+router.get('/projects/:id/update', isLoggedIn, projectsCtrl.showUpdate)
 
 router.post('/', projectsCtrl.create)
 router.delete('/projects/:id', projectsCtrl.deleteProject)
--- routes/supplies.js
+++ routes/supplies.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let router = express.Router();
 
+var passport = require("passport")
+const isLoggedIn = require("../config/auth")
 const suppliesCtrl = require('../controllers/supplies')
 
-router.post('/projects/:id/supplies', suppliesCtrl.create)
+router.post('/projects/:id/supplies', isLoggedIn, suppliesCtrl.create)
 
 module.exports = router
